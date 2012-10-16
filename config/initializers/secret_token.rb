d669e75823247d848e9454da81b2526ec414ea76
Brad Milne <brad.milne@getelevate.com>
2012-10-15 20:47:00 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iconette::Application.config.secret_token = 'b9758a4537622c0d4bffb3f26f5949191c963877eda5be1152a37ec0ca979ace8f494f765240061e4cff6ed00312c68185eb0bba7d0cd0c6a7332e82db11a342'
